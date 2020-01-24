190564b69b129e42d3a017b12321ba2e1f6a1bba
AgileCloudManager <Agile.Cloud.Manager@GreenRiverIT.com>
2020-01-24 12:01:37 -0800

    error handling

--- call-to-module/kubernetes-admin-client-aws-call-to-module/pipeline-k8sadmin-instance-apply.py
+++ call-to-module/kubernetes-admin-client-aws-call-to-module/pipeline-k8sadmin-instance-apply.py
@@ -4,20 +4,25 @@
 import subprocess
 import sys
 import os 
- 
-path_to_k8sadmin_iam_keys=os.environ['TF_VAR_PATH_TO_K8SADMIN_IAM_KEYS']
-print("path_to_k8sadmin_iam_keys is: "+path_to_k8sadmin_iam_keys)
-igw_id=sys.argv[2]
-print("igw_id is: "+igw_id)
-sg_id=sys.argv[3]
-print("sg_id is: "+sg_id)
-cidr_holder=sys.argv[4]
-print("cidr_holder is: "+cidr_holder)
 
-subprocess.run("terraform init", shell=True, check=True)
 
-applyCommand="terraform apply -auto-approve -var-file="+path_to_k8sadmin_iam_keys+"awspublickey.tfvars -var-file="+path_to_k8sadmin_iam_keys+"awsvpcmeta.tfvars -var-file="+path_to_k8sadmin_iam_keys+"awskeymeta.tfvars -var igw_acm_host="+igw_id+" -var sgid_acm_nodes="+sg_id+" -var cidr_subnet_acm="+cidr_holder
+try: 
+    path_to_k8sadmin_iam_keys=os.environ['TF_VAR_PATH_TO_K8SADMIN_IAM_KEYS']
+    print("path_to_k8sadmin_iam_keys is: "+path_to_k8sadmin_iam_keys)
+    igw_id=sys.argv[2]
+    print("igw_id is: "+igw_id)
+    sg_id=sys.argv[3]
+    print("sg_id is: "+sg_id)
+    cidr_holder=sys.argv[4]
+    print("cidr_holder is: "+cidr_holder)
 
-print("applyCommand is: " +applyCommand)
+    subprocess.run("terraform init", shell=True, check=True)
 
-subprocess.run(applyCommand, shell=True, check=True)
+    applyCommand="terraform apply -auto-approve -var-file="+path_to_k8sadmin_iam_keys+"awspublickey.tfvars -var-file="+path_to_k8sadmin_iam_keys+"awsvpcmeta.tfvars -var-file="+path_to_k8sadmin_iam_keys+"awskeymeta.tfvars -var igw_acm_host="+igw_id+" -var sgid_acm_nodes="+sg_id+" -var cidr_subnet_acm="+cidr_holder
+
+    print("applyCommand is: " +applyCommand)
+
+    subprocess.run(applyCommand, shell=True, check=True)
+except Exception as e:
+    print("stdout output:\n", e.output)
+    sys.exit()
--- call-to-module/kubernetes-admin-client-aws-call-to-module/pipeline-k8sadmin-instance-destroy.py
+++ call-to-module/kubernetes-admin-client-aws-call-to-module/pipeline-k8sadmin-instance-destroy.py
@@ -4,13 +4,17 @@
   
 import subprocess 
 import os 
- 
-igw_id = ""
-sg_id = ""
-cidr_holder = ""
 
-subprocess.run("terraform init", shell=True, check=True)
+try: 
+    igw_id = ""
+    sg_id = ""
+    cidr_holder = ""
 
-path_to_k8sadmin_iam_keys=os.environ['TF_VAR_PATH_TO_K8SADMIN_IAM_KEYS']
+    subprocess.run("terraform init", shell=True, check=True)
 
-subprocess.run("terraform destroy -auto-approve -var-file="+path_to_k8sadmin_iam_keys+"awspublickey.tfvars -var-file="+path_to_k8sadmin_iam_keys+"awsvpcmeta.tfvars -var-file="+path_to_k8sadmin_iam_keys+"awskeymeta.tfvars -var igw_acm_host="+igw_id+" -var sgid_acm_nodes="+sg_id+" -var cidr_subnet_acm="+cidr_holder, shell=True, check=True)
+    path_to_k8sadmin_iam_keys=os.environ['TF_VAR_PATH_TO_K8SADMIN_IAM_KEYS']
+
+    subprocess.run("terraform destroy -auto-approve -var-file="+path_to_k8sadmin_iam_keys+"awspublickey.tfvars -var-file="+path_to_k8sadmin_iam_keys+"awsvpcmeta.tfvars -var-file="+path_to_k8sadmin_iam_keys+"awskeymeta.tfvars -var igw_acm_host="+igw_id+" -var sgid_acm_nodes="+sg_id+" -var cidr_subnet_acm="+cidr_holder, shell=True, check=True)
+except Exception as e:
+    print("stdout output:\n", e.output)
+    sys.exit()
